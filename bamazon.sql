137a0ea18846c59f7c189f86df5b9912157f1261
Kevin Do <do.kevin.vo@gmail.com>
2018-09-20 21:28:20 -0700

    Adds initial database

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100)  NOT NULL,
+    price DECIMAL(8, 2) NULL,
+    stock_quantity INTEGER
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Blade Runner 2049", "Movies, Music & Games", 19.99, 700),
+       ("Mistborn: The Final Empire", "Books & Audible", 17.99, 200),
+       ("Lady Bird", "Movies, Music & Game", 14.99, 400),
+       ("Witcher 3: Wild Hunt Complete Edition", "Movies, Music & Game", 59.99, 327),
+       ("Perry Ellis Men's Slim Fit Dress Pants", "Clothing, Shoes & Jewelry", 49.99, 100),
+       ("Charmin Ultra Soft Toilet Paper, Family Mega Roll", "Food and Grocery", 29.99, 1000),
+       ("Burley Design Bee Bike Trailer, Yellow", "Sports & Outdoors", 299.00, 50),
+       ("American Gods", "Books & Audible", 19.99, 80),
+       ("Monstress", "Books & Audible", 9.99, 300),
+       ("Kellogg's Fruit Loops Cereal", "Food and Grocery", 12.82, 5000);
+               
+SELECT * FROM products;
