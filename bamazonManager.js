04ddfb0a5a29786094da933849f034eacbc51699
Kevin Do <do.kevin.vo@gmail.com>
2018-09-23 18:39:07 -0700

    Adds ability to increase an item's stock and update db

--- bamazonManager.js
+++ bamazonManager.js
@@ -27,6 +27,10 @@ function menuOptions() {
             productsForSale();
         } else if(mode.option === `View Low Inventory`) {
             lowInventory();
+        } else if(mode.option === `Add to Inventory`) {
+            addInventory();
+        } else if(mode.option === `Add New Product`) {
+            newProduct();
         }
     });
 }
@@ -61,4 +65,46 @@ Stock: ${show.stock_quantity}
         })
     })
     connection.end();
+}
+
+function addInventory() {
+    var data = `SELECT * FROM products`;
+    connection.query(data, function(err,res) {
+        if(err) throw err;
+        inquirer.prompt({
+            name: `itemId`,
+            type: `list`,
+            message: `To add more of the item, select its corresponding ID number:`,
+            choices: function() {
+                var itemIdArray = [];
+                for (let i = 0; i < res.length; i++) {
+                    itemIdArray.push(`${res[i].item_id}`);
+                }
+                return itemIdArray;
+            }
+        }).then((selected) => {
+            j = Number(selected.itemId - 1);
+            var k = res[j];
+            inquirer.prompt({
+                name: `amount`,
+                type: `input`,
+                message: `How much would you like to add to the stock of \"${k.product_name}\", which currently has ${k.stock_quantity} units?`
+            }).then((add) => {
+                if (add.amount === "" || isNaN(add.amount) || add.amount % 1 !== 0 ) {
+                    console.log(`You did not input an integer number. Please run \'node bamazonManager.js\' again`);
+                } else {
+                    var newQuantity = Number(add.amount) + k.stock_quantity;
+                    var updateData = `UPDATE products SET stock_quantity = ${newQuantity} where item_id = ${k.item_id};`;
+                    connection.query(updateData, function() {
+                        console.log(`There are now ${newQuantity} units of \'${k.product_name}\' in stock.`)
+                    });
+                }
+                connection.end();
+            });
+        });
+    })
+}
+
+function newProduct() {
+    console.log(`WIP`);
 }
