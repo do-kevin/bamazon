385cfbc854e2041292a3275acd7d33e707ab9bd1
Kevin Do <do.kevin.vo@gmail.com>
2018-09-24 21:21:30 -0700

    Updates schema

--- schema.sql
+++ schema.sql
@@ -31,3 +31,7 @@ SELECT DISTINCT departments.department_id, departments.department_name, departme
 FROM departments INNER JOIN products ON (departments.department_name = products.department_name)  
 WHERE (departments.department_name = products.department_name) ORDER BY department_id ASC;
 
+
+
+
+
